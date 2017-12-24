f8bac25d5c7fff81ea54ba225ef27f3300a156fb
Isiloki <l.4nanas@gmail.com>
2017-12-24 19:07:03 +0100

    Ajoutez des fichiers projet.

---
+++ quaddxmas.DAL.API/PersonnageDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quaddxmas.DAL.API
+{
+    public class PersonnageDAO
+    {
+    }
+}
---
+++ quaddxmas.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="quaddxmas.UI.MvcApplication" Language="C#" %>
